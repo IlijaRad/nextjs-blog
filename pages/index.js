cdbc4eff5a4a4c9e39af58446b3de6351a83c899
unknown <ilija.radivojevic237@gmail.com>
2022-06-25 19:05:44 +0200

    Added about page, template page and page titles

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 const HomePage = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Head>
+        <title>My blog</title>
+        <meta name="description" value="This is my blog" />
+      </Head>
+      <div>Hello</div>;
+    </>
+  );
 };
 
 export default HomePage;
